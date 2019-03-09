386713d3f24daa9ec543e75e4be9459946cf3254
Richard Cook <rcook@rcook.org>
2019-03-09 11:52:39 -0800

    Implement eval

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,42 @@ mod expr {
     }
 }
 
+fn eval(e: Box<expr::Expr>) -> Option<i32> {
+    use expr::Expr::*;
+    match *e {
+        Val(value) => Some(value),
+        Add(left, right) => {
+            let l = eval(left)?;
+            let r = eval(right)?;
+            Some(l + r)
+        }
+        Sub(left, right) => {
+            let l = eval(left)?;
+            let r = eval(right)?;
+            Some(l - r)
+        }
+        Mul(left, right) => {
+            let l = eval(left)?;
+            let r = eval(right)?;
+            Some(l * r)
+        }
+        Div(left, right) => {
+            let r = eval(right)?;
+            if r == 0 { return None }
+            let l = eval(left)?;
+            Some(l / r)
+        }
+    }
+}
+
 fn main() {
     use expr::*;
-    let e = div(mul(val(2), add(val(3), val(4))), val(2));
-    println!("e={:?}", e)
+    {
+        let e = div(mul(val(2), add(val(3), val(4))), val(2));
+        println!("e={:?}", eval(e)) // Divide by 2: displays "Some(7)"
+    }
+    {
+        let e = div(mul(val(2), add(val(3), val(4))), val(0));
+        println!("e={:?}", eval(e)) // Divide by 0: displays "None"
+    }
 }
